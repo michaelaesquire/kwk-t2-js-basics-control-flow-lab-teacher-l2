906fbfd0046d4ef6529e811e0f854ac236cbfec8
Michaela Olson <mco44@cornell.edu>
2018-05-08 21:28:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     returnValue = "This one is on me!";
   }
-  else if (someValue > 2000) {
-    returnValue = "I will gladly take your thirty bucks."
+  else if (someValue > 2000 && someValue <= 2500) {
+    returnValue = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue >2500) {
+    
   }
   return returnValue;
-  
+
 }
